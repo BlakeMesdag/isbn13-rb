ba1555aad0aafd3ca48354d00699b1175838c934
BlakeMesdag <696663+BlakeMesdag@users.noreply.github.com>
2025-01-06 23:39:28 -0500

    Update tests to use a confirmed ISBN example, implement checksums

--- lib/isbn13.rb
+++ lib/isbn13.rb
@@ -4,7 +4,31 @@ require_relative "isbn13/version"
 
 module Isbn13
   class Error < StandardError; end
-  
+
   def self.checksum(input)
+    if input.is_a?(String)
+      checksum_from_string(input)
+    else
+      checksum_from_integer(input)
+    end
+  end
+
+  def self.checksum_from_string(input)
+    checksum_from_integer(input.delete("^0-9").to_i)
+  end
+
+  def self.checksum_from_integer(input)
+    digits = input.digits.reverse # Take all the digits separately, reverse since digits does a mod(10) extraction into an array
+
+    # Only take the first 12 digits
+    digits = digits.take(12)
+
+    # Separate the even and odd numbers
+    evens = digits.slice((0..).step(2))
+    odds = digits.slice((1..).step(2))
+
+    sums = (evens.sum + (odds.sum * 3)) % 10
+
+    (10 - sums) % 10
   end
 end
--- spec/isbn13_spec.rb
+++ spec/isbn13_spec.rb
@@ -7,29 +7,29 @@ RSpec.describe Isbn13 do
 
   context "#checksum works for integer inputs" do
     it "works for integers without a checksum" do
-      expect(Isbn13.checksum(978010101010)).to eq(7)
+      expect(Isbn13.checksum(978014300723)).to eq(4)
     end
 
     it "works for integers with an existing checksum" do
-      expect(Isbn13.checksum(9780101010107)).to eq(7)
+      expect(Isbn13.checksum(9780143007234)).to eq(4)
     end
   end
 
   context "#checksum works for string inputs" do
     it "works for strings without a checksum" do
-      expect(Isbn13.checksum("978010101010")).to eq(7)
+      expect(Isbn13.checksum("978014300723")).to eq(4)
     end
 
     it "works for strings with a checksum" do
-      expect(Isbn13.checksum("9780101010107")).to eq(7)
+      expect(Isbn13.checksum("9780143007234")).to eq(4)
     end
 
     it "works for strings with dashes" do
-      expect(Isbn13.checksum("978-0-101-01010")).to eq(7)
+      expect(Isbn13.checksum("978-0-143-00723")).to eq(4)
     end
 
     it "works for strings with dashes with a checksum" do
-      expect(Isbn13.checksum("978-0-101-01010-7")).to eq(7)
+      expect(Isbn13.checksum("978-0-143-00723-4")).to eq(4)
     end
   end
 end
